d3c8c5aa66d6e821399bb92a3de4a563202bbb8e
Josh Waldron <jwaldron@thejwal.com>
2013-05-23 20:23:17 -0500

    getting layout configured

--- BabyBook/App_Start/BundleConfig.cs
+++ BabyBook/App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace BabyBook
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*", "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Bootstrap.css", 
                 "~/Content/Bootstrap-responsive.css",
